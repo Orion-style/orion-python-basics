c2ca375629210c8519f92bcd976584832999c9c6
Orion_style <microsoftazod@gmail.com>
2026-05-12 08:40:18 +0300

    Add day 1 Python practice

---
+++ day-01/profile.py
@@ -0,0 +1,8 @@
+name = input("Имя: ")
+age = input("Возраст: ")
+goal = input("Цель: ")
+
+print("----Анкета студента----")
+print("Имя:", name)
+print("Возраст:", age)
+print("Цель:", goal)
---
+++ day-01/student_card.py
@@ -0,0 +1,9 @@
+name = "Orion"
+direction = "Backend-Developer"
+goal = "Попасть на стажировку"
+study_days = 30
+
+print("Имя:", name)
+print("Направление:", direction)
+print("Цель:", goal)
+print("План обучения:", study_days, "дней")
